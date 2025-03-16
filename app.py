61cede12bc36c5c257a587a378bd09df6449814e
SaiAnilKumarDeyyala <darlingss014@gmail.com>
2025-03-16 21:10:13 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import cv2
+import time
+from twilio.rest import Client
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+
+# Twilio API Settings
+# this code for
+account_sid = '<account_sid>'
+auth_token = '<auth_token>'
+twilio_number = +13344497885
+mynumber = +919999999999  # your number
+client = Client(account_sid,auth_token,)
+
+total_count = 0
+t = 60 * 60
+def sms_alert():
+ message = client.messages.create(
+ body=f"{count} students are in the classRoom and total of {total_count} in the class till now",
+ from_=twilio_number,
+ to=+918340096052
+ )
+ print(f'{count} people exist in class')
+ print(message.sid)
+ time.sleep(t)
+
+# To capture video from webcam.
+cap = cv2.VideoCapture(0)
+# To use a video file as input
+# cap = cv2.VideoCapture(r"C:\Users\Admin\Downloads\Office - 69952.mp4")
+while True:
+ # Read the frame
+ _, img = cap.read()
+ count = 0
+ # Convert to grayscale
+ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ # Detect the faces
+ faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+ font = cv2.FONT_HERSHEY_SIMPLEX
+ # Draw the rectangle around each face
+ for (x, y, w, h) in faces:
+  cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+  count += 1
+ #sms_alert()
+ cv2.putText(img, f'{count}', (50, 50), font, 1, (0, 255, 255),2, cv2.LINE_4)
+ # Display
+ cv2.imshow('img', img)
+ # Stop if escape key is pressed
+ k = cv2.waitKey(30) & 0xff
+ if k == 'Q':
+  break
+# Release the VideoCapture object
+cap.release()
